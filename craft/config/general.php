d44e2c9e17ef3f8ab2789ef2ff9d5316929e9712
Plunkett112358 <plunkett.meghann@gmail.com>
2015-02-05 12:22:00 -0500

    xfbgvhn

--- craft/config/general.php
+++ craft/config/general.php
@@ -14,7 +14,7 @@ return array(
 		 'resourceTrigger' => 'files',
 		 'useEmailAsUsername' => true,
 		 'autoLoginAfterAccountActivation' => true,
-		 'postLoginRedirect' => 'final-details'
+		 'postLoginRedirect' => ''
     
     
       
